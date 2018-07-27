ab742d11503bcfb4eb22c6973214ab071c516b58
JNRuan <jnruan@gmail.com>
2018-07-27 10:02:22 +1000

    Commented out dev test globals, and data load timing output.

--- inst/shiny-examples/visualopie/dataLoader.R
+++ inst/shiny-examples/visualopie/dataLoader.R
@@ -7,14 +7,14 @@ source("dashboard.R")
 
 dataLoader <- function(input, output, session) {
   #' Server function to handle operations instigated by the load button
-  #' 
+  #'
   #' @param input
   #' @param output
   #' @param session
-  
+
   # Run folder selection
   folderSelect(input, output, session)
-  
+
   # Parse and visualise when load button hit.
   observeEvent(input$loadButton, {
     main_start <- Sys.time()
@@ -26,7 +26,7 @@ dataLoader <- function(input, output, session) {
         logPath <<- paste(logPath, OPIE_DEFAULT_LOGS, sep = "")
       }
     }
-    
+
     # print("*** START ***")
     # start_time <- Sys.time()
     # Empty list of folders found in selected device/folder
@@ -41,28 +41,28 @@ dataLoader <- function(input, output, session) {
     # print(paste0("Get folder paths - Time taken:", end_time - start_time))
     # Emtpy list of faciliator logs as data tables
     facilRawTbls <- list(NULL, NULL)
-    
+
     # Vector of paths to logs from robot separated into types
     logsFromRobot <- c()
-    
+
     # start_time <- Sys.time()
     # Individual facilitator logs as data tables in a list
     logs_from_facil <- facilLogs(input, output, session, folderList)
     # end_time <- Sys.time()
     # print(paste0("Get facil logs - Time taken:", end_time - start_time))
-    
+
     # start_time <- Sys.time()
     # Vector of paths to all robot logs by type
     logsFromRobot <- robotLogs(folderList)
     # end_time <- Sys.time()
     # print(paste0("Get robot logs - Time taken:", end_time - start_time))
-    
+
     # Table of memory game data
     memoryGameTbl <<- NULL
-    
+
     # Table for main logs
     mainLogsTbl <<- NULL
-    
+
     # start_time <- Sys.time()
     if(length(logs_from_facil) == 0) {
       showUINotification("No facilitator logs found.", msgType = "error")
@@ -72,7 +72,7 @@ dataLoader <- function(input, output, session) {
     }
     # end_time <- Sys.time()
     # print(paste0("Facil table - Time taken:", end_time - start_time))
-    
+
     # start_time <- Sys.time()
     if(length(logsFromRobot$memory) == 0) {
       showUINotification("No robot logs found.", msgType = "error")
@@ -88,21 +88,21 @@ dataLoader <- function(input, output, session) {
     # Basic log statistics
     numLogs <- length(logs_from_facil) + sum(lengths(logsFromRobot)) - length(logsFromRobot$rejected)
     infoDialogue(input, output, session, numLogs)
-    
+
     # Number of Profiles
     if(!is.null(memoryGameTbl)) {
       userCount <- nrow(memoryGameTbl %>% group_by(name) %>% summarise(n_distinct(name)))
       numUsersDialogue(input, output, session, userCount)
-      
+
       langCount <- nrow(memoryGameTbl %>% group_by(language) %>% summarise(n_distinct(name)))
       numLangDialogue(input, output, session, langCount)
     }
-    
+
     # Update Activity Count + Plot the associated pie chart
     callModule(activity_stats_update, "activity", data = logsFromRobot)
     # end_time <- Sys.time()
     # print(paste0("Log stats - Time taken:", end_time - start_time))
-    
+
     # start_time <- Sys.time()
     # Start all visualisations (see function in this script - 'dataLoader.R')
     vizData = list(facilRawTbls[[1]], facilRawTbls[[2]], memoryGameTbl, mainLogsTbl)
@@ -118,30 +118,30 @@ dataLoader <- function(input, output, session) {
 
 facilTable <- function(logs_from_facil) {
   #' Create data tables for developer processing from a list of all paths to facilitator logs.
-  #' 
+  #'
   #' @param logs_from_facil - list of facilitator logs as direct paths.
 
-  start_time <- Sys.time()
+  # start_time <- Sys.time()
   stateData <<- rbindlist(logs_from_facil)
   stateData[, "min_child"] <- -1
   stateData[, "max_child"] <- -1
-  setnames(stateData, c("time","state","num_children","score","notes", "session", "duration", 
+  setnames(stateData, c("time","state","num_children","score","notes", "session", "duration",
                         "dur_min", "min_child", "max_child"))
-  
-  end_time <- Sys.time()
-  print(paste0("> Facil Tbl state data - Time taken:", end_time - start_time))
-  
-  start_time <- Sys.time()
+
+  # end_time <- Sys.time()
+  # print(paste0("> Facil Tbl state data - Time taken:", end_time - start_time))
+
+  # start_time <- Sys.time()
   reducedData <<- collapsetbl(stateData)
-  
+
   reducedData <<- reducedData%>%
     group_by(state)%>%
     mutate(dur_min = duration/60)%>%
     filter(duration <= (mean(duration)+2*sd(duration)) & duration >= (mean(duration)-2*sd(duration)) & duration >= 2.0)
-  
-  end_time <- Sys.time()
-  print(paste0("> Facil table - reduced data - Time taken:", end_time - start_time))
-  
+
+  # end_time <- Sys.time()
+  # print(paste0("> Facil table - reduced data - Time taken:", end_time - start_time))
+
   tables <- list(stateData, reducedData)
   return(tables)
 }
@@ -149,15 +149,15 @@ facilTable <- function(logs_from_facil) {
 memoryLogs <- function (logs) {
   #' Builds the data table consisting of all memory game logs with valid data entries.
   #' Logs without a level played are excluded.
-  #' 
+  #'
   #' @param logs - vector of paths to each memory game log file.
   #' @return data.table of memory game information.
-  
+
   mylist.names <- c("name", "language", "level", "correct", "incorrect", "session")
   memlog <- vector("list", length(mylist.names))
   names(memlog) <- mylist.names
-  
-  start_time <- Sys.time()
+
+  # start_time <- Sys.time()
   for(id in 1:length(logs$memory)) {
     txt <- tolower(readLines(logs$memory[id]))
     len <- length(search_phrase("level", txt))
@@ -170,78 +170,78 @@ memoryLogs <- function (logs) {
       memlog$session <- c(memlog$session, rep(sub("_log", "", basename(logs$memory[id])), each=len))
     }
   }
-  
+
   memTbl <- data.table(memlog$name, memlog$language, memlog$level, memlog$correct, memlog$incorrect, memlog$session) %>%
     setNames(c("name", "language", "level", "correct", "incorrect", "session"))
-  
-  end_time <- Sys.time()
-  print(paste0("> Memlogs table creation - Time taken:", end_time - start_time))
-  
-  start_time <- Sys.time()
-  memTbl <- memTbl %>% 
+
+  # end_time <- Sys.time()
+  # print(paste0("> Memlogs table creation - Time taken:", end_time - start_time))
+
+  # start_time <- Sys.time()
+  memTbl <- memTbl %>%
     mutate(score = as.numeric(sub(".*scored ", "", level))) %>%
     mutate(level = sub(".*level ", "", level)) %>%
     mutate(level = as.numeric(sub("\ :(.*)", "", level))) %>%
-    mutate(name = sub(".*: ", "", name)) %>%  
+    mutate(name = sub(".*: ", "", name)) %>%
     mutate(correct = sub(".*: ", "", correct)) %>%
     mutate(incorrect = sub(".*: ", "", incorrect)) %>%
     mutate(language = sub(".*: ", "", language))
-  
-  end_time <- Sys.time()
-  print(paste0(">Memlogs mutate table - Time taken:", end_time - start_time))
-  
-  start_time <- Sys.time()
+
+  # end_time <- Sys.time()
+  # print(paste0(">Memlogs mutate table - Time taken:", end_time - start_time))
+
+  # start_time <- Sys.time()
   # Order the logs by language
   memTbl <- memTbl[order(memTbl$language, memTbl$level),]
-  end_time <- Sys.time()
-  print(paste0(">Mem logs order table - Time taken:", end_time - start_time))
-  
+  # end_time <- Sys.time()
+  # print(paste0(">Mem logs order table - Time taken:", end_time - start_time))
+
   return(memTbl)
-  
+
 }
 
 mainTable <- function(logs) {
-  
+
   mainTbl <- NULL
-  
+
   mylist.names <- c("name", "type", "value", "times", "session")
   mainlog <- vector("list", length(mylist.names))
   names(mainlog) <- mylist.names
-  
+
   for(id in 1:length(logs$main)) {
     txt <- tolower(readLines(logs$main[id]))
-    
+
     langid <- search_phrase("languages used:", txt, id = 1)
     actid <- search_phrase("games played", txt, id = 1)
     endid <- search_phrase("-----------", txt, id = 1)
-    
+
     langs <- txt[(langid+1):(actid-1)]
     langs <- langs[langs != ""]
     acts <- txt[(actid+1):(endid-1)]
     acts <- acts[acts != ""]
-  
+
     for (lid in 1:length(langs)) {
       mainlog$name <- c(mainlog$name, sub(".*: ", "", search_phrase("student", txt)))
       mainlog$type <- c(mainlog$type, "language")
       mainlog$value <- c(mainlog$value, sub("\t.*", "", langs[lid]))
       mainlog$times <- c(mainlog$times, sub("*.\t", "", langs[lid])) # TODO: FIX REGEX
-      mainlog$session <- c(mainlog$session, sub("_log", "", basename(logs$main[id]))) 
+      mainlog$session <- c(mainlog$session, sub("_log", "", basename(logs$main[id])))
     }
-    
+
     for (aid in 1:length(acts)) {
       mainlog$name <- c(mainlog$name, sub(".*: ", "", search_phrase("student", txt)))
       mainlog$type <- c(mainlog$type, "activity")
       mainlog$value <- c(mainlog$value, sub("\t.*", "", acts[aid]))
       mainlog$times <- c(mainlog$times, sub("*.\t", "", acts[aid])) # TODO: FIX REGEX
-      mainlog$session <- c(mainlog$session, sub("_log", "", basename(logs$main[id]))) 
-      
+      mainlog$session <- c(mainlog$session, sub("_log", "", basename(logs$main[id])))
+
     }
-    
+
     mainTbl <<- data.table(mainlog$name, mainlog$type, mainlog$value, mainlog$times, mainlog$session) %>%
       setNames(c("name", "type", "value", "times", "session"))
-    
+
   }
-  
+
   return(mainTbl)
 
 }
@@ -249,12 +249,12 @@ mainTable <- function(logs) {
 
 robotLogs <- function(folderList) {
   #' Gather robot logs and divide up by type.
-  #' 
+  #'
   #' @param folderList - list of folders to search through, including selected root.
   #' @return Vector of logs divided into activities.
-  
+
   logs <- c()
-  
+
   # start_time <- Sys.time()
   # Search folders for logs
   for(i in 1:length(folderList)) {
@@ -269,7 +269,7 @@ robotLogs <- function(folderList) {
   mylist.names <- c("memory", "repetition", "story", "main", "rejected")
   gamelogs <- vector("list", length(mylist.names))
   names(gamelogs) <- mylist.names
-  
+
   # start_time <- Sys.time()
   # Compile valid log paths into gamelogs
   if(length(logs) > 0) {
@@ -289,17 +289,17 @@ robotLogs <- function(folderList) {
       }
     }
   }
-  
+
   gamelogs <<- gamelogs
   # end_time <- Sys.time()
   # print(paste0(">Robot logs compile game logs - Time taken:", end_time - start_time))
-  
+
   return(gamelogs)
 }
 
 infoDialogue <- function(input, output, session, numLogs) {
   #' Output info box for logs
-  #' 
+  #'
   #' @param numLogs - the number of logs loaded.
   output$logsLoadedBox <- renderInfoBox({
     infoBox(
@@ -333,7 +333,7 @@ numLangDialogue <- function(input, output, session, numLangs) {
 facilLogs <- function(input, output, session, folderList) {
   #' Get all log filenames with full path, excluding audio files.
   #' Searches all folders including the selected root folder.
-  #' 
+  #'
   #' @param input - Shiny inputs
   #' @param output - Shiny outputs
   #' @param session - Current Shiny session
@@ -341,7 +341,7 @@ facilLogs <- function(input, output, session, folderList) {
   # Search folders for facilitator logs and get log names
   logNames <- NULL
   folderLogs <- NULL
-  start_time <- Sys.time()
+  # start_time <- Sys.time()
   for(i in 1:length(folderList)) {
     # Facilitator logs are .txt files (other logs don't have a file ext)
     tempLogNames <- grep(list.files(path = folderList[i], full.names = FALSE), pattern = "*.txt", value = T)
@@ -354,15 +354,15 @@ facilLogs <- function(input, output, session, folderList) {
     # Compile names
     tempLogNames <- sub("_log.*", "", tempLogNames)
     logNames <- c(logNames, tempLogNames)
-    
+
     # Compile logs
     folderLogs <- c(folderLogs, tempLogs)
   }
-  end_time <- Sys.time()
-  print(paste0("> Facillogs - grab log names and paths - time taken:", end_time - start_time))
+  # end_time <- Sys.time()
+  # print(paste0("> Facillogs - grab log names and paths - time taken:", end_time - start_time))
   # Remove duplicates
   # All actual logs that are parsed
-  start_time <- Sys.time()
+  # start_time <- Sys.time()
   allLogs <- c()
   if(length(logNames) > 0) {
     duplicates = duplicated(logNames)
@@ -374,23 +374,23 @@ facilLogs <- function(input, output, session, folderList) {
   } else {
     allLogs <- folderLogs
   }
-    
+
   # Names of log files for session marker
   logNames <- unique(logNames)
-  end_time <- Sys.time()
-  print(paste0(">Facillogs - remove duplicates - time taken:", end_time - start_time))
-  
-  start_time <- Sys.time()
+  # end_time <- Sys.time()
+  # print(paste0(">Facillogs - remove duplicates - time taken:", end_time - start_time))
+
+  # start_time <- Sys.time()
   # Combine logs into one data table, logs == list(data, data, ...), data == data.table
   logs <- lapply(allLogs, fread, sep = "\t")
-  end_time <- Sys.time()
-  print(paste0(">Facillogs - combine logs into one dtable - time taken:", end_time - start_time))
-  
-  start_timme <- Sys.time()
+  # end_time <- Sys.time()
+  # print(paste0(">Facillogs - combine logs into one dtable - time taken:", end_time - start_time))
+
+  # start_timme <- Sys.time()
   # Account for time per log and append
   if(length(logs) > 0) {
     logs <- time_continuity(logs)
-    
+
     for(id in 1:length(logs)) {
       logs[[id]] <- logs[[id]]%>%
         mutate(session = logNames[id])%>%
@@ -398,8 +398,8 @@ facilLogs <- function(input, output, session, folderList) {
         mutate(dur_min=duration/60)
     }
   }
-  end_time <- Sys.time()
-  print(paste0(">Facillogs - timecont + mutate - time taken:", end_time - start_time))
+  # end_time <- Sys.time()
+  # print(paste0(">Facillogs - timecont + mutate - time taken:", end_time - start_time))
   return(logs)
 }
 
@@ -414,7 +414,7 @@ folderSelect <- function(input, output, session) {
     handlerExpr = {
       if (input$directory > 0) {
         # condition prevents handler execution on initial app launch
-        
+
         # launch the directory selection dialog with initial path read from the widget
         path = choose.dir(default = readDirectoryInput(session, 'directory'))
 
@@ -428,7 +428,7 @@ folderSelect <- function(input, output, session) {
 
 collapsetbl <- function(dataTable) {
   #' Creates a reduced tibble by grouping each state's contiguous block of entries into one entry
-  #' 
+  #'
   #' @param dataTable - data table containing raw data from multiple logs concatenated together
   #' @return a reduced data table with contiguous state-wise data collapsed into a single entry
 
@@ -436,72 +436,72 @@ collapsetbl <- function(dataTable) {
   Y <- cumsum(c(1, X$lengths[-length(X$lengths)]))
   reduced <- dataTable[Y]
   for(id in 1:length(Y)) {
-    
+
     tempDurations <- dataTable$duration[Y[id]:(Y[id] + (X$lengths[id] - 1))]
     tempChildren <- dataTable$num_children[Y[id]:(Y[id] + (X$lengths[id] - 1))]
-    
+
     reduced[id] <- reduced[id] %>%
       mutate(duration = sum(tempDurations), max_child = max(tempChildren), min_child = min(tempChildren))
   }
-  
+
   return(reduced)
 }
 
 start_visualisation <- function(data) {
   #' Start visualisations, after data logs processed.
   #' This is also where we ensure whether plots can or cannot be loaded.
-  #' 
+  #'
   #' @param data - a list of data tables derived from loaded logs.
-  
+
   if(!is.null(data[[1]]) & !is.null(data[[2]])) {
     # Raw data tables
     callModule(raw_data_table, "raw", data = data[[1]])
     callModule(raw_reduced_table, "raw", data = data[[2]])
-    
+
     # Dashboard Visualisations
     callModule(dash_activity_duration_plot, "dashboard", data = data[[2]])
     callModule(dash_activity_ratio_plot, "dashboard", data = data[[2]])
     callModule(dash_activity_comparison, "dashboard", data = data[[2]])
     callModule(dash_box_child_plot, "dashboard", data = data[[2]])
     callModule(dash_child, "dashboard", data = data[[1]])
-    
+
     # Activity Visuliations
     # Note, activity pie charts for percentage of activities as a ratio is done as count is updated at data load.
     callModule(activity_tbl, "activity", data = data[[2]])
     callModule(plot_activity_time, "activity", data = data[[2]])
-    
+
     # Success
     showUINotification("Facilitator logs visualised!")
   }
-  
+
   if(!is.null(data[[3]])) {
     # Raw memory game data table
     callModule(raw_memory_table, "raw", data = data[[3]])
-    
+
     # Language Visualisations
     callModule(lang_table_viz, "language", data = data[[3]])
     callModule(lang_stack_viz, "language", data = data[[3]])
     callModule(lang_bar_plot, "language", data = data[[3]])
     callModule(plotWord, "language", data = data[[3]])
-    
+
     # Update stack visualiser
     callModule(update_stack_bars, "language")
     callModule(update_word_bar, "language")
-    
+
     # Activity Language Visualisations
     callModule(plot_avg_words, "activity", data = data[[3]])
     callModule(plot_word_level_scatter, "activity", data = data[[3]])
-    
+
     # User Page - update selector
     # callModule(update_user_selector, "user", data = data[[3]])
-    
+
     # User Page - Visualisers
     callModule(plot_user_language_stats, "user", data = data[[3]])
-    
+
     # User Page Update, update the selector first (Default - have all users selected)
     callModule(update_user_selector, "user", data = data[[3]])
     callModule(update_user_stats, "user", data = data[[3]])
-    
+
     # Success
     showUINotification("Robot logs visualised!")
   }
