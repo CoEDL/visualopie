d6113f72fed82a6814bbed95384895ff9efc9efa
JNRuan <jnruan@gmail.com>
2018-07-30 17:10:30 +1000

    Incorporated browser launch into run.

--- inst/shiny-examples/visualopie/runApp.R
+++
@@ -1,6 +0,0 @@
-###########################
-## Run Shiny App in Browser
-###########################
-
-# Run app from shiny working directory, launches default browser.
-runApp('./inst/shiny-examples/visualopie', launch.browser = TRUE)
