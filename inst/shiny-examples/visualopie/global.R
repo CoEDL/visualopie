fa4f353a34808e9797beadce2e009bf01474a445
JNRuan <jnruan@gmail.com>
2018-08-01 09:34:37 +1000

    Cleaned out non-production necessary globals.

--- inst/shiny-examples/visualopie/global.R
+++ inst/shiny-examples/visualopie/global.R
@@ -1,12 +1,8 @@
 ## global.R ## stores global variables
 
 # Paths for testing
-# logPath <- "C://Users//Aninda Saha//Google Drive//University of Queensland//Winter Research 2018//Winter Projects 2018//Opie logs"
-
 OPIE_DEFAULT_LOGS <- "/Internal storage/Android/data/com.UQ.NgukurrOpie/files"
 OPIE_DEFAULT_LOGS_WIN <- "//Internal storage//Android//data//com.UQ.NgukurrOpie//files"  # Needs investigation due to security.
-
-logNames <- c()
 
 # Standard sizing options
 WIDTH <- 250
