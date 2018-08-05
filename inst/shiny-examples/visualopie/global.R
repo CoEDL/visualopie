e94e17564d39de1f642ff897536cf5d35cf2aa2d
JNRuan <jnruan@gmail.com>
2018-08-06 09:34:17 +1000

    Change robot path to new opie path.

--- inst/shiny-examples/visualopie/global.R
+++ inst/shiny-examples/visualopie/global.R
@@ -1,8 +1,8 @@
 ## global.R ## stores global variables
 
 # Paths for testing
-OPIE_DEFAULT_LOGS <- "/Internal storage/Android/data/com.UQ.NgukurrOpie/files"
-OPIE_DEFAULT_LOGS_WIN <- "//Internal storage//Android//data//com.UQ.NgukurrOpie//files"  # Needs investigation due to security.
+OPIE_DEFAULT_LOGS <- "/Internal storage/Android/data/com.UQ.ILR/files"
+OPIE_DEFAULT_LOGS_WIN <- "//Internal storage//Android//data//com.UQ.ILR//files"  # Needs investigation due to security.
 
 # Standard sizing options
 WIDTH <- 250
