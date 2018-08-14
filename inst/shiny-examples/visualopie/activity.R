bf906e2b261f7bb56decc2c2d6d28ca3aa67f73d
JNRuan <jnruan@gmail.com>
2018-08-14 10:47:36 +1000

    Documentation and minor cleanup of code in activity.R and dashboard.R

--- inst/shiny-examples/visualopie/activity.R
+++ inst/shiny-examples/visualopie/activity.R
@@ -1,17 +1,26 @@
-## activity.R ## displays activity centric statistical information
+###############################
+## Activity Visualisation Page
+###############################
 
 source('language.R')
 
+######
+## UI
+######
+
 activity_ui <- function(id) {
-  #' User interface for the activity data page
-  #' 
-  #' @param id - the id used for the parent frame that will contain this page
-  
-  ns <- NS(id) # Creating namespace allocator for dash_ui
-  
+  #' User interface for the activity data page.
+  #'
+  #' The activity page should show activity usage information, and general results based on each activity if
+  #' applicable.
+  #'
+  #' @param id - namespace id for activity functions.
+
+  # Namespace
+  ns <- NS(id)
+
   tabItem(
     tabName = id,
-    
     fluidRow(
       class = "activityRow",
       box(
@@ -40,25 +49,34 @@ activity_ui <- function(id) {
       )
     ),
     tags$head(tags$style(
-      ".activityRow { margin-top: 2rem; margin-bottom: 2rem}"
+      ".activityRow { margin-top: 1rem; margin-bottom: 1rem}"
       ))
     )
-  
+
 }
+
+
+##################
+## Data Rendering
+##################
 
 activity_stats_update <- function(input, output, session, data) {
   #' Update activity status information boxes. Default 0 if no data loaded.
-  #' 
-  #' @param data - vector of logs from robot.
-  
+  #'
+  #' Each game makes a new log per attempt.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - Vector of containing logs from robot.
+
   totalAct <- 0
   numMemory <- 0
   numStory <- 0
   numRep <- 0
-  
+
   if(!is.null(data)) {
-    # df <- data %>% mutate(dur_min = round(dur_min, 2)) %>% filter(state %in% c("memory", "story", "repetition"))
-    # Get total entries per game + total
+    # Get total recorded logs per game + total
     totalAct <- length(data[[1]]) + length(data[[2]]) + length(data[[3]])
     numMemory <- length(data[[1]])
     numRep <- length(data[[2]])
@@ -70,76 +88,79 @@ activity_stats_update <- function(input, output, session, data) {
       "Total Activities Run", totalAct, icon = icon("table"), color = "aqua", fill = TRUE
     )
   })
-  
   output$memoryPlayedBox <- renderInfoBox({
     infoBox(
       "Memory Game", numMemory, icon = icon("graduation-cap"), color = "teal", fill = TRUE
     )
   })
-  
   output$storiesPlayedBox <- renderInfoBox({
     infoBox(
       "Story", numStory, icon = icon("book"), color = "purple", fill = TRUE
     )
   })
-  
   output$repsPlayedBox <- renderInfoBox({
     infoBox(
       "Repetition", numRep, icon = icon("microphone"), color = "maroon", fill = TRUE
     )
   })
-  
-  # Plot associated pie chart for break down of activity use from robot.
+
   plot_activity_ratio(input, output, session, data)
 }
 
 derive_time_data <- function(input, output, session, data) {
-  #' Derive time data from master tables.
-  #' 
+  #' Derive time data from master tables. Sourced from facilitator logs only.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
   #' @param data - master table to derive from.
-  #' @return data table/frame of derived time data.
-  summary <- data %>% group_by(state) %>% 
+  #' @return data table/frame of derived time data per activity.
+
+  summary <- data %>% group_by(state) %>%
     filter(state %in% c("memory", "repetition", "story")) %>%
-    summarise(MinDuration = round(min(duration), 2), 
-              MaxDuration = round(max(dur_min), 2), 
+    summarise(MinDuration = round(min(duration), 2),
+              MaxDuration = round(max(dur_min), 2),
               AvgDuration = round(mean(dur_min), 2))
-  
+
   return(summary)
 }
 
 activity_tbl <- function(input, output, session, data) {
-  #' Render activity summary table.
-  #' 
+  #' Render activity time summary table.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
   #' @param data - master data table from loaded data (facilitator logs).
-  summary <- derive_time_data(input, output, session, data) %>% 
+
+  summary <- derive_time_data(input, output, session, data) %>%
     setnames(c("Activity", "Shortest Time (secs)", "Longest Time (mins)", "Average Time (mins)"))
-  
+
   output$activityTbl <- DT::renderDataTable({
-    datatable(summary, 
+              datatable(summary,
               class = "display cell-border",
               options = list(dom = 't'))
   })
-} 
+}
 
 plot_activity_time <- function(input, output, session, data) {
-  #' Server function that generates plot of times broken down by group. 
-  #' 
-  #' @param input
-  #' @param output
-  #' @param session
+  #' Server function that generates plot of times broken down by group.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
   #' @param data - main data table of compiled log data.
-  
+
   # Combine and group durational data for each activity for plotting
   summary <- derive_time_data(input, output, session, data) %>%
     mutate(MinDuration = MinDuration / 60) %>%
     melt(id = "state")
-  
-  # Render bar plot for durations, by group
+
   output$activityTimeBar <- renderPlot({
     ggplot(summary, aes(x = state, y = value, fill = variable)) +
-      geom_col(position = "dodge") + 
-      labs(x = "Activity", y = "Time (minutes)", fill = '', title = "Time per Activity") + 
-      theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5), 
+      geom_col(position = "dodge") +
+      labs(x = "Activity", y = "Time (minutes)", fill = '', title = "Time per Activity") +
+      theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'),
             axis.text.x=element_text(size = 12, face = 'bold')
             )
@@ -147,29 +168,28 @@ plot_activity_time <- function(input, output, session, data) {
 }
 
 plot_activity_ratio <- function(input, output, session, data) {
-  #' Server function to generate pie for number of activities.
-  #' 
-  #' @param input
-  #' @param output
-  #' @param session
+  #' Generate pie for number of activities.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
   #' @param data - main data table of compiled log data.
-  
+
   # Get counts for activities
   numMemory <- length(data[[1]])
   numRep <- length(data[[2]])
   numStory <- length(data[[3]])
-  
+
   # Form data table for chart
   activity <- c("Memory", "Repetition", "Story")
   n <- c(numMemory, numRep, numStory)
   dt <- data.table(activity, n) %>% mutate(percentage = round((n / sum(n) * 100), 2))
 
-  # Plot pie chart with percentages
   output$activityNumbersPie <- renderPlot({
     ggplot(data = dt, aes(x = '', y = n, fill = activity)) +
-      geom_bar(width = 1, stat = "identity") + 
+      geom_bar(width = 1, stat = "identity") +
       coord_polar('y', start = 0, direction = 1) +
-      labs(x = '', y = '', title = "Activity Popularity (% of Total Activities)", fill = "Activity") + 
+      labs(x = '', y = '', title = "Activity Popularity (% of Total Activities)", fill = "Activity") +
       theme(axis.ticks = element_blank(),
             axis.text = element_blank(),
             plot.title = element_text(size = 16, face = 'bold', hjust = 0.5)
@@ -181,23 +201,24 @@ plot_activity_ratio <- function(input, output, session, data) {
 
 plot_avg_words <- function(input, output, session, data) {
   #' Plot the average words correct/incorrect by level.
+  #'
   #' Plot is a grouped bar plot.
-  #' 
-  #' @param input
-  #' @param output
-  #' @param session
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
   #' @param data - master data table of memory game logs to derive.
 
-  wordCorrectness <- data %>% 
-    group_by(level) %>% 
+  # Calculate average words correct vs incorrect per level
+  wordCorrectness <- data %>%
+    group_by(level) %>%
     summarise(mean_cor = mean(score), mean_inc = mean(level - score)) %>%
     setnames(c("Level", "Avg Correct Guesses", "Avg Incorrect Guesses")) %>%
     melt(id = "Level")
-  
-  # Render bar plot for average correct/incorrect, grouped by level
+
   output$avgCorrectPlot <- renderPlot({
     ggplot(wordCorrectness, aes(x = Level, y = value, fill = variable)) +
-      geom_col(position = "dodge") + 
+      geom_col(position = "dodge") +
       scale_x_continuous(breaks = pretty(wordCorrectness$Level, n = 10)) +
       scale_y_continuous(breaks = pretty(wordCorrectness$Level, n = 10)) +
       labs(x = "Level", y = "Average Num. Words", title = "Average Words Correct VS Incorrect by Level") +
@@ -210,24 +231,23 @@ plot_avg_words <- function(input, output, session, data) {
 
 plot_word_level_scatter <- function(input, output, session, data) {
   #' Plots a scatter plot of words correct/incorrect by level.
-  #' 
+  #'
   #' @param input - Shiny input
   #' @param output - Shiny output
   #' @param session - Shiny session
   #' @param data - raw data from load
-  
+
   # Derive table of correctness each word/level
   scatterDT <<- data %>% group_by(level, session) %>% mutate(numCorrect = score, numIncorrect = level - score) %>%
     gather(correctness, value, c(numCorrect, numIncorrect))
-  
-  # Plot scatter
+
   output$wordCorrectScatter <- renderPlot({
     ggplot(data = scatterDT, aes(x = level, y = value, shape = correctness, colour = correctness)) +
-      geom_point(size = 2) + 
-      geom_jitter() + 
-      geom_smooth(method=lm) + 
-      scale_x_continuous(breaks = pretty(scatterDT$level, n = max(scatterDT$level))) + 
-      scale_y_continuous(breaks = pretty(scatterDT$value, n = max(scatterDT$value))) + 
+      geom_point(size = 2) +
+      geom_jitter() +
+      geom_smooth(method=lm) +
+      scale_x_continuous(breaks = pretty(scatterDT$level, n = max(scatterDT$level))) +
+      scale_y_continuous(breaks = pretty(scatterDT$value, n = max(scatterDT$value))) +
       labs(x = "Level", y = "", title = "Words Correct vs Incorrect per Level") +
       theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'),
--- inst/shiny-examples/visualopie/dashboard.R
+++ inst/shiny-examples/visualopie/dashboard.R
@@ -1,15 +1,24 @@
-## dashboard.R ## main page that displays overall information
+#################################
+## General Facilitator Dashboard
+#################################
+
+# TODO: Needs redesign as information is not particularly targetted.
+
+######
+## UI
+######
 
 dash_ui <- function(id) {
-  #' User interface for the dashboard
-  #' 
-  #' @param id - the id used for the parent frame that will contain the dashboard ui
-  
-  ns <- NS(id) # Creating namespace allocator for dash_ui
-  
+  #' User interface for the general facilitator dashboard.
+  #'
+  #' @param id - namespace id for activity functions.
+
+  # Namespace
+  ns <- NS(id)
+
   tabItem(
     tabName = id,
-    
+
     fluidRow(
       class = "dashRow",
       box(
@@ -18,7 +27,6 @@ dash_ui <- function(id) {
         column(6, plotOutput(ns("activity_durations")))
       )
     ),
-    
     fluidRow(
       class = "dashRow",
       box(
@@ -28,116 +36,130 @@ dash_ui <- function(id) {
         column(6, plotOutput(ns("activity_compare")))
       )
     ),
-    
     tags$style(".dashRow { margin-top: 2rem; margin-bottom: 2rem}")
   )
 }
 
 dash_box_child_plot<- function (input, output, session, data) {
   #' Displays duration based data including total runtime in each state
-  #' 
-  #' @param input
-  #' @param output
-  #' @param session
-  #' @param data - dataframe which includes info including time, state, score, num_children, max_child
-  
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - dataframe derived from facilitator logs.
+
   output$boxChildPlot <- renderPlot({
-    
+    # Violin plot for child distribution per activity state
     ggplot(data, aes(x=state, y=max_child, fill = state)) +
       geom_violin() +
-      labs(title="Boxplot of max children distribution in each activity") + 
+      labs(title="Max children distribution in each activity") +
       theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'))
   })
 }
 
 dash_child <- function(input, output, session, data) {
-  #' Server function that generates plot of dur_min vs num_child for each activity. 
-  #' 
-  #' @param input
-  #' @param output
-  #' @param session
-  #' @param data - dataframe which includes info including time, state, score, num_children, max_child
-  
-  
-  output$childDistribution <- renderPlot({
+  #' Generates plot of minimum durations vs number of children for each activity.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - dataframe derived from facilitator logs.
 
-    children_per_activity <- data%>%
-      filter(state!="login" & state!="out_of_app" & num_children!=0)%>%
-      group_by(state,num_children)%>%
-      summarize(time=sum(dur_min))
-    
+  children_per_activity <- data%>%
+    filter(state!="login" & state!="out_of_app" & num_children!=0)%>%
+    group_by(state,num_children)%>%
+    summarize(time=sum(dur_min))
+
+  output$childDistribution <- renderPlot({
+    # Plot for children per state recorded in facilitator log
     ggplot(children_per_activity,aes(x=num_children,y=time,fill = state)) +
       geom_col() +
       facet_grid(. ~ state) +
       labs(title = "Children distribution per game",x="Children number", y="Total duration (min)") +
-      theme_bw() + 
+      theme_bw() +
       theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'))
-      
   })
 }
 
 dash_activity_duration_plot <- function(input, output, session, data) {
+  #' Generates plot of duration per activity.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - dataframe derived from facilitator logs.
+
+  df <- data %>% mutate(dur_min = round(dur_min, 2))
 
   output$bar_state_duration <- renderPlot({
-    df <- data %>% mutate(dur_min = round(dur_min, 2))
     # Plot total duration (in minutes) of OPIE states.
-    ggplot(data = df, aes(state, dur_min, fill = state)) + 
-      geom_col() + 
-      stat_summary(fun.y = sum, aes(label = ..y.., group = state, vjust = -.5), geom = "text") + 
-      labs(x = "OPIE State", y = "Duration (minutes)", title = "Total time spent in OPIE by state") + 
+    ggplot(data = df, aes(state, dur_min, fill = state)) +
+      geom_col() +
+      stat_summary(fun.y = sum, aes(label = ..y.., group = state, vjust = -.5), geom = "text") +
+      labs(x = "OPIE State", y = "Duration (minutes)", title = "Total time spent in OPIE by state") +
       theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'))
   })
 }
 
 dash_activity_comparison <- function(input, output, session, data) {
-  
+  #' Generates plot of duration x number of children x robot state
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - dataframe derived from facilitator logs.
+
   output$activity_compare <- renderPlot({
     df <- data %>% mutate(dur_min = round(dur_min, 2)) %>% filter(state %in% c("memory", "story", "repetition"))
     df <- df %>% mutate_at(scale, .vars = vars(dur_min))
-    
+
     # Duration x NumChildren x State
     ggplot(data = df, aes(max_child, dur_min, shape = state, color = state)) +
       geom_point(size = 2) +
-      geom_smooth(span = 0.9, method=lm, se=TRUE, fullrange=TRUE) + 
-      labs(x = "Max Num Children", y = "Duration (minutes)", title = "Duration in activity per grouping of children.") + 
+      geom_smooth(span = 0.9, method=lm, se=TRUE, fullrange=TRUE) +
+      labs(x = "Max Num Children", y = "Duration (minutes)", title = "Duration in activity per grouping of children.") +
       theme(plot.title=element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.title=element_text(size = 12, face = 'bold'))
   })
 }
 
 dash_activity_ratio_plot <- function(input, output, session, data) {
-  
+  #' Generates activity ratio plot.
+  #'
+  #' @param input - Shiny inputs.
+  #' @param output - Shiny outputs.
+  #' @param session - Shiny session.
+  #' @param data - dataframe derived from facilitator logs.
+
+  # Sum all state durations by time(minutes)
+  df <- data %>% group_by(state) %>% summarise(dur_min = sum(dur_min))
+
+  # Aggregate for when OPIE running an activity
+  in_activity <- filter(df, state %in% c("memory", "story", "repetition")) %>% summarise(total_minutes = sum(dur_min))
+  in_activity["state"] <- "In Activity"
+
+  # Aggregate when OPIE not running an activity
+  non_activity <- filter(df, state %in% c("login", "out_of_app")) %>% summarise(total_minutes = sum(dur_min))
+  non_activity["state"] <- "Out of Activity"
+
+  activity_ratio <- bind_rows(in_activity, non_activity)
+  activity_ratio <- mutate(activity_ratio,
+                           percentage = (activity_ratio$total_minutes / sum(activity_ratio$total_minutes)) * 100)
+
   output$activity_durations <- renderPlot({
-    # Sum all state durations by time(minutes)
-    df <- data %>% group_by(state) %>% summarise(dur_min = sum(dur_min))
-    
-    # Aggregate when OPIE running an activity
-    in_activity <- filter(df, state %in% c("memory", "story", "repetition")) %>% summarise(total_minutes = sum(dur_min))
-    in_activity["state"] <- "In Activity"
-    
-    # Aggregate when OPIE not running an activity
-    non_activity <- filter(df, state %in% c("login", "out_of_app")) %>% summarise(total_minutes = sum(dur_min))
-    non_activity["state"] <- "Out of Activity"
-    
-    activity_ratio <- bind_rows(in_activity, non_activity)
-    activity_ratio <- mutate(activity_ratio, 
-                             percentage = (activity_ratio$total_minutes / sum(activity_ratio$total_minutes)) * 100)
-    # print(activity_ratio)
-    
-    # Plot ratio in/out of activity
-    ggplot(data = activity_ratio, aes(x = "", y = percentage, fill = state)) + 
-      geom_bar(width = 1, stat = "identity") + 
-      coord_polar("y", start = 0, direction = 1) + 
-      scale_fill_manual(values = c("#fee8c8", "#e34a33")) + 
-      labs(x = "", y = "", title = "Time spent in activities VS out of activities") + 
-      geom_text(aes(y = percentage, label = round(percentage, 2)), position = position_stack(vjust = 0.5)) + 
+    # Plot ratio in/out of activity as a pie chart.
+    ggplot(data = activity_ratio, aes(x = "", y = percentage, fill = state)) +
+      geom_bar(width = 1, stat = "identity") +
+      coord_polar("y", start = 0, direction = 1) +
+      scale_fill_manual(values = c("#fee8c8", "#e34a33")) +
+      labs(x = "", y = "", title = "Time spent in activities VS out of activities") +
+      geom_text(aes(y = percentage, label = round(percentage, 2)), position = position_stack(vjust = 0.5)) +
       theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5),
             axis.ticks = element_blank(),
             axis.text = element_blank()
       )
-    
   })
 }
